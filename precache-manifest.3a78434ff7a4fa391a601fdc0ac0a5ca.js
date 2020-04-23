9f6cf9a7dc3075d1ae01a0cb37d9cf5db2150873
CHANDHRU R <44549194+chan2412@users.noreply.github.com>
2020-04-23 11:30:21 +0530

    Updates

---
+++ precache-manifest.3a78434ff7a4fa391a601fdc0ac0a5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68faca8b1bdf81cae0561fd1e87524ff",
+    "url": "/calcibmi/index.html"
+  },
+  {
+    "revision": "e7737d829720f361dde4",
+    "url": "/calcibmi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a856828a284f59c3ff69",
+    "url": "/calcibmi/static/js/2.b6a2c0e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/calcibmi/static/js/2.b6a2c0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7737d829720f361dde4",
+    "url": "/calcibmi/static/js/main.ded0dfed.chunk.js"
+  },
+  {
+    "revision": "947828b6b9866e8f1b4f",
+    "url": "/calcibmi/static/js/runtime-main.f36eb41c.js"
+  }
+]);
